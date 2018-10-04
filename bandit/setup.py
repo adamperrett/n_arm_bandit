bab36f520ec1c7721126ccb29a32cdba02fa14f5
such-a-git <adamperrett3142@gmail.com>
2018-10-04 18:29:24 +0100

    Further changes to setup coming from spinn_utils

--- bandit/setup.py
+++ bandit/setup.py
@@ -1,4 +1,27 @@
 from setuptools import setup, find_packages
+from collections import defaultdict
+import os
+
+
+# Build a list of all project modules, as well as supplementary files
+main_package = "spinn_utilities"
+extensions = {".aplx", ".boot", ".cfg", ".json", ".sql", ".template", ".xml",
+              ".xsd"}
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        if ext in extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append(filename)
 
 # **HACK** spynnaker doesn't have __version__ set properly
 # therefore >= 3.0.0, < 4.0.0 doesn't work correctly
